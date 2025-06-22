fa4413206012f4f01f5fb6c6ddada21e4a212e6d
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2025-06-22 14:59:58 -0400

    Tasa BER

--- Lab4/Matlab/lab4.m
+++ Lab4/Matlab/lab4.m
@@ -1,11 +1,10 @@
-% Rango de Eb/N0 en decibelios
-EbN0_dB = 0:11;                     % de 0 a 11 dB
-EbN0_lin = 10.^(EbN0_dB / 10);      % conversión a escala lineal
+% Rango en decibelios
+EbN0_dB = 0:11;
+EbN0_lin = 10.^(EbN0_dB / 10);
 
-% BER teórica para BPSK
+% BER teórico BPSK
 BER_theoretical = 0.5 * erfc(sqrt(EbN0_lin));
 
-% Graficar en escala semilogarítmica
 semilogy(EbN0_dB, BER_theoretical, 'b-o', 'LineWidth', 2);
 grid on;
 xlabel('E_b/N_0 [dB]');
